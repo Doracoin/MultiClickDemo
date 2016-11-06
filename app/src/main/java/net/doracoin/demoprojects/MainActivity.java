15196c8f2fcaf566aac9fe06f953015997314add
Yangw at home PC <776146966@qq.com>
2016-11-06 16:17:02 +0800

    增加了“双击返回键退出”
    Signed-off-by: at home PC <776146966@qq.com>

--- app/src/main/java/net/doracoin/demoprojects/MainActivity.java
+++ app/src/main/java/net/doracoin/demoprojects/MainActivity.java
@@ -2,6 +2,7 @@ package net.doracoin.demoprojects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -74,6 +75,31 @@ public class MainActivity extends AppCompatActivity {
         }
         mToast.setText(text);
         mToast.show();
+    }
+
+    int back_click_count = 0;
+    long back_click_time = 0;
+
+    /**
+     * 双击返回键退出
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                if (System.currentTimeMillis() - back_click_time < 300) {
+                    back_click_count++;
+                } else {
+                    back_click_count = 1;
+                    toast("双击返回键退出");
+                }
+                back_click_time = System.currentTimeMillis();
+                if (back_click_count == 2) {
+                    finish();
+                }
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 }
